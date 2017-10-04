f31b22761647a10a627adcef0d5ab92b395898c8
tainea <tainea@hotmail.com>
2017-10-04 22:30:08 +0200

    Update program-najdi_TP01.py

--- tp01/program-najdi_TP01.py
+++ tp01/program-najdi_TP01.py
@@ -28,14 +28,14 @@ print ("you have BEF ", bef, " and it is equal to EUR ", euro, ".")
 # et la décompose en billets de 10E, en pièces de 2E et de 1E.
 
 montant = float(input("input your amount "))
-rest1 = float(montant % 10)
+reste1 = float(montant % 10)
 nbDixEuro = int(montant // 10)
 
-rest2 = float(rest1 % 2)
+reste2 = float(reste1 % 2)
 nbDeuxEuro = int(rest1 // 2)
 
-rest3 = float(rest2 % 1)
-nbUnEuro = int(rest2 // 1)
+reste3 = float(reste2 % 1)
+nbUnEuro = int(reste2 // 1)
     
 print ("your input amount EUR ", montant, " can be decomposed into : ")
 print (nbDixEuro, " billets de 10EUR")
